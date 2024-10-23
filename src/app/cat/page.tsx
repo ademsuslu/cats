0cbd0800f3111ec1e53627f0022d6e1e1f89c3ad
ademsuslu <ademsuslu9080@gmail.com>
2024-10-23 10:49:17 +0300

    refactor:was navbar in icons

--- src/app/cat/page.tsx
+++ src/app/cat/page.tsx
@@ -2,7 +2,7 @@ export default async function  CatsPage() {
 
   
   return (
-    <div className="p-8">
+    <div>
             Cats    
     </div>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,17 @@
+import { BiSolidCat } from "react-icons/bi";
+import { PiDog } from "react-icons/pi";
+
 import Link from 'next/link'
 import React from 'react'
+
 
 const Navbar = () => {
   return (
     <div className='space-x-8 flex w-full h-16  items-center justify-between'>
         <Link href="/">Animals</Link>
         <div className='flex items-center justify-between gap-2'>
-            <Link href="/cat">Kedi</Link>
-            <Link href="/dog">Köpek</Link>
+            <Link href="/cat"><BiSolidCat className="w-4 h-4" /></Link>
+            <Link href="/dog"><PiDog className="w-4 h-4"/></Link>
         </div>
     </div>
   )
